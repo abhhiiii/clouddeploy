7bf208f1e0e10da72114e6d241748cab09a73909
Abhishek1 Singh <Abhishek1.Singh@datametica.com>
2021-02-15 15:56:07 +0530

    updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 
+env_name=process.env.name2
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'automatically';
-  res.send(`i have updated ${name}!`);
+  res.send(`i have updated ${name}! ${env_name}`);
 });
 
 const port = process.env.PORT || 8080;
